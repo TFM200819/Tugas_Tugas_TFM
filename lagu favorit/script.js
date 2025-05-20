c52f167e8d275c97ed3c4d9ef1effecc558818a9
TFM200819 <131218456+TFM200819@users.noreply.github.com>
2025-05-20 14:57:59 +0700

    Tugas DDK3

---
+++ lagu favorit/script.js
@@ -0,0 +1,9 @@
+
+//membuat data array berisi 3 lagu favorit (judul, penyanyi, jumlah_likes, jumlah_play)
+
+var lagu = [
+    ['Mangu', 'Fourtwnty', 4000, 8000],
+    ['Slalu Ada di Nadi', 'B.C.L', 1500, 2300],
+    ['Lesung Pipi', 'Raim LaOde', 2440, 5400],
+];
+
